2df8b11bca019fdf6bd42cada9bdd53dc32da22c
Akshay R. Kapadia <akshayrajankapadia@gmail.com>
2020-03-22 15:09:14 -0400

    Added more to the template

--- Coronavirus/src/countries.py
+++ Coronavirus/src/countries.py
@@ -1,5 +1,41 @@
+class Earth(object):
+    """
+    Class that represents the Earth.
+
+    Args:
+        population (int),
+        countries (Country[])
+    """
+
+    def __init__(self, population, countries):
+        super(Earth, self).__init__()
+        pass
+
+    # TODO: Return the percentage of cases resulting in deaths
+    def getPercentDead():
+        pass
+
+    # TODO: Return the percentage of cases resulting in full recoveries
+    def getPercentRecovered():
+        pass
+
+    # TODO: Return the percentage of people infected
+    def getPercentInfected():
+        pass
+
+    # TODO: Return the country with the most active cases
+    def getMostInfectedCountry():
+        pass
+
+
 class Country(object):
-    """Class that represents a coutry."""
+    """
+    Class that represents a coutry.
+
+    Args:
+        name (string),
+        population (int)
+    """
 
     # TODO: Initialize class variables
     def __init__(self, name, population):
@@ -8,7 +44,17 @@ class Country(object):
 
 
 class InfectedCountry(Country):
-    """Class that represents an infected country."""
+    """
+    Class that represents an infected country.
+
+    Args:
+        name (string),
+        population (int),
+        cases (int),
+        recovered (int),
+        deaths (int),
+        activeCases (int)
+    """
 
     # TODO: Initialize class variables
     def __init__(self, name, population, cases, recovered, deaths, activeCases):
